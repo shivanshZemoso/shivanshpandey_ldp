a87b3fd391790175bc74f3994e9629c4c535f01c
shivansh <shivansh.pandey@zemosolabs.com>
2023-11-29 12:51:49 +0530

    initializing the project

---
+++ contiq/src/main/java/com/contiq/ContiqApplication.java
@@ -0,0 +1,13 @@
+package com.contiq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContiqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContiqApplication.class, args);
+	}
+
+}
---
+++ contiq/src/test/java/com/contiq/ContiqApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.contiq;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ContiqApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
